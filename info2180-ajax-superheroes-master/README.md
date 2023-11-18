0855fcf13564d5a327134fdc46b5db155ba0685f
Christian <christiandouglas57@gmail.com>
2023-11-18 12:34:31 -0500

    Finished Exercise 3

---
+++ info2180-ajax-superheroes-master/README.md
@@ -0,0 +1,2 @@
+# AJAX Superheroes Starter Files
+This is Lab 4 for Christian Douglas
---
+++ info2180-ajax-superheroes-master/app.js
@@ -0,0 +1,30 @@
+function searchList()
+{
+    var result = document.getElementById("result");
+    var val = document.getElementById("hero-input");
+    console.log("hello")
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if(req.readyState === XMLHttpRequest.DONE){
+            if(req.status === 200)
+            {
+                result.innerHTML = req.responseText
+            } 
+            else
+            {
+                console.log("Error")
+            }
+        }
+    }
+    req.open("GET",`http://localhost/info2180-lab4/superheroes.php?query=${val.value}`,false)
+
+    req.send();
+}
+
+
+window.onload = function() {
+    console.log("Page loaded");
+    var  but = document.getElementById("search-btn");
+    but.addEventListener("click", searchList);
+  
+};
